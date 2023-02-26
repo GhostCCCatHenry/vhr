ea0842e1469f22fb167b19a9ca81c4dc828ed0aa
houzhifang <houzhifang@tencent.com>
2023-02-26 17:35:23 +0800

    20230226 learning

---
+++ vhr/mailserver/src/test/java/org/javaboy/mailserver/basic.java
@@ -0,0 +1,11 @@
+package org.javaboy.mailserver;
+
+public class basic {
+    public static void main(String[] args) {
+        /*
+        transient关键字：指定某些内容不需要被序列化。
+        序列化：内容将会被保存至存储设备或远程rpc传输。
+
+         */
+    }
+}
